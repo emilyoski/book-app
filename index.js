3c237491e5c3cbf6eb3494ee4fddd767eb64b2f5
Emily Olszewski <92195356+emilyoski@users.noreply.github.com>
2023-09-05 20:44:29 -0700

    add postgres

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-// postgres connection for books by id
+const pgp = require('pg-promise');
+const db = pgp('postgres://username:password@host:port/database');
+let bookData;
+
+function generateRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+db.many('SELECT * FROM books;')
+  .then((data) => {
+    bookData = data.values
+    console.log('DATA:', data.values)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
+
 
 app.get('/api/books', (_req, response) => {
-  response.send('Harry Potter');
+  let idx = generateRandomNumber(0, 6)
+  response.json(bookData[idx]);
 });
 
 const PORT = 3001;
